4bd8f5f423b3ef8a50ba7a467d1aab12cfecd7b2
Karsten Schwarz <karsten@Falme.fritz.box>
2020-11-03 20:50:08 +0100

    created type

--- lib/puppet/provider/mac_profile/mac_profile.rb
+++ lib/puppet/provider/mac_profile/mac_profile.rb
@@ -4,6 +4,17 @@ require 'puppet/resource_api/simple_provider'
 
 # Implementation for the mac_profile type using the Resource API.
 class Puppet::Provider::MacProfile::MacProfile < Puppet::ResourceApi::SimpleProvider
+  def initialize
+    require 'puppet/util/plist'
+    # require 'cfpropertylist'
+  end
+
+  def canonicalize(context, resources)
+    resources.each do |resource|
+      resource[:uuid] = resource[:uuid].upcase unless resource[:uuid].nil?
+    end
+  end
+
   def get(context)
     context.debug('Returning pre-canned example data')
     [
@@ -20,13 +31,23 @@ class Puppet::Provider::MacProfile::MacProfile < Puppet::ResourceApi::SimpleProv
 
   def create(context, name, should)
     context.notice("Creating '#{name}' with #{should.inspect}")
+    create_or_update(context, name, should)
   end
 
   def update(context, name, should)
+    validate_should(should)
     context.notice("Updating '#{name}' with #{should.inspect}")
+    create_or_update(context, name, should)
   end
 
   def delete(context, name)
     context.notice("Deleting '#{name}'")
+  end
+
+  def create_or_update(context, name, should)
+    validate_should(should)
+  end
+
+  def validate_should(should)
   end
 end
